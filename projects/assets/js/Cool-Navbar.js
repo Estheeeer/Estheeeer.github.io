021623145a9ce26e4eb5f1fd45d6a29350ceae48
Esther <estherxz43@gmail.com>
2022-03-23 20:56:44 -0400

    update

--- projects/assets/js/Cool-Navbar.js
+++ projects/assets/js/Cool-Navbar.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 console.log("ready!");
 $(window).scroll(function() {
-    if ($("#main-navbar").offset().top > 50) {
+    if ($("#main-navbar").offset().top > 10) {
         $("#main-navbar").addClass("navbar-fixed-top");
     } else {
         $("#main-navbar").removeClass("navbar-fixed-top");
