a5005ede925ef537cf5e5b39402165c21409763a
Esther <estherxz43@gmail.com>
2022-04-06 09:22:20 -0400

    update website

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+document.onreadystatechange = function() {
+            if (document.readyState !== "complete") {
+                document.querySelector(
+                  "body").style.visibility = "hidden";
+                document.querySelector(
+                  "#container").style.visibility = "visible";
+            } else {
+                document.querySelector(
+                  "#container").style.display = "none";
+                document.querySelector(
+                  "body").style.visibility = "visible";
+            }
+        };
