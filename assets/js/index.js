9e3140ebc4ec6225d1834e5922b0cd197ff8a52b
Esther <estherxz43@gmail.com>
2022-04-07 18:50:42 -0400

    update work

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,3 +11,58 @@ document.onreadystatechange = function() {
                   "body").style.visibility = "visible";
             }
 };
+
+
+// projects  
+const projects = [
+  {
+    nr: '01',
+    name: 'Disequilibrium Room',
+    link: 'https://www.youtube.com/watch?v=XU1Cvdl-Rgw',
+    imgURL = 'https://media.istockphoto.com/vectors/image-preview-icon-picture-placeholder-for-website-or-uiux-design-vector-id1222357475?k=20&m=1222357475&s=170667a&w=0&h=YGycIDbBRAWkZaSvdyUFvotdGfnKhkutJhMOZtIoUKY='
+  },
+  {
+    nr: '02',
+    name: 'AM, PM'
+    link: 'https://itp.nyu.edu/shows/winter2020/am-pm/',
+    imgURL = 'https://media.istockphoto.com/vectors/image-preview-icon-picture-placeholder-for-website-or-uiux-design-vector-id1222357475?k=20&m=1222357475&s=170667a&w=0&h=YGycIDbBRAWkZaSvdyUFvotdGfnKhkutJhMOZtIoUKY='
+
+  },
+  {
+    nr: '03',
+    name: 'placehoder-01',
+    link: 'https://estherzhang.com/',
+    imgURL = 'https://media.istockphoto.com/vectors/image-preview-icon-picture-placeholder-for-website-or-uiux-design-vector-id1222357475?k=20&m=1222357475&s=170667a&w=0&h=YGycIDbBRAWkZaSvdyUFvotdGfnKhkutJhMOZtIoUKY='
+  },
+  {
+    nr: '04',
+    name: 'placehoder-02',
+    link: 'https://estherzhang.com/',
+    imgURL = 'https://media.istockphoto.com/vectors/image-preview-icon-picture-placeholder-for-website-or-uiux-design-vector-id1222357475?k=20&m=1222357475&s=170667a&w=0&h=YGycIDbBRAWkZaSvdyUFvotdGfnKhkutJhMOZtIoUKY='
+    
+  },
+  {
+
+const app = document.getElementById('app');
+const imgBaseURL = 'https://www.florin-pop.com/images/100Days100PRojects/Day ';
+
+projects.forEach(project => {
+  // const title = `${project.name} - #${project.nr}`;
+  const title = `${project.name}`;
+
+  // const imgURL = imgBaseURL + project.nr + '.jpg';
+  
+  const projectEl = document.createElement('a');
+  projectEl.classList.add('project');
+  projectEl.href = project.link;
+  projectEl.target = '_blank';
+
+  projectEl.innerHTML = `
+        <img src="${imgURL}" alt="${title}"/>
+    <p>
+      ${title}
+    </p>
+    `;
+
+  app.appendChild(projectEl);
+});
